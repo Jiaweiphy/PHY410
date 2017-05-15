f5116be11ffd07c830f397323361355da9e8558e
Jiawei Wang <jwang97@buffalo.edu>
2017-05-15 13:33:34 -0400

    Changes.

--- Assignment6/A6P1.py
+++ Assignment6/A6P1.py
@@ -8,11 +8,23 @@ def V (r) :
     y = r[1]
     return -x*x/2 - y*y/2 + (x*x + y*y)*(x*x + y*y)/4
 
+def nV (r) :
+    x = r[0]
+    y = r[1]
+    return x*x/2 + y*y/2 - (x*x + y*y)*(x*x + y*y)/4
+
 def dV(r) :
     x = r[0]
     y = r[1]
     x = -x + x*x*x + x*y*y
     y = -y + y*y*y + y*x*x
+    return np.array( [x,y] )
+
+def ndV(r) :
+    x = r[0]
+    y = r[1]
+    x = x - x*x*x - x*y*y
+    y = y - y*y*y - y*x*x
     return np.array( [x,y] )
 
 
@@ -23,4 +35,6 @@ gtol = input( " Enter desired accuracy: ")
 V_min = 0.0
 iterations = 0
 res = scipy.optimize.fmin_bfgs(f=V, fprime=dV,x0=r, gtol=gtol)
+ma = scipy.optimize.fmin_bfgs(f=nV, fprime=ndV,x0=r, gtol=gtol)
 print res
+print ma
